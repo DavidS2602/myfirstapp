e8919cf52ef2e55661b60b40a29340b7e658eab8
davids2602 <DavidS260202@proton.me>
2024-08-25 17:58:42 -0600

    refactor: move business logic to service and optimize HTTP methods- Transferred business logic from controllers to respective services- Optimized HTTP method usage in controllers- Improved separation of concerns

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
-@Controller({})
+@Controller('/tasks')
 export class TasksController {
   tasksService: TasksService;
   constructor(tasksService: TasksService) {
     this.tasksService = tasksService;
   }
-  @Get('/tasks')
+  @Get('')
   getAllTasks() {
     return this.tasksService.getAllTasks();
+  }
+  @Post() //{title: 'primer tarea', status: false} -> {title:'tarea 2',
+  //status:true
+  creatTask() {
+    return this.tasksService.createTask();
+  }
+
+  @Put()
+  updateTask() {
+    return this.tasksService.updateTask();
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTask();
+  }
+
+  @Patch() // {title: 'arreglo 1', status: true} -> {status: false}
+  updateTaskStatus() {
+    return this.tasksService.updateTaskStatus();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,4 +5,20 @@ export class TasksService {
   getAllTasks() {
     return ['Task 1', 'Task 2', 'Task 3'];
   }
+
+  createTask() {
+    return 'This task was created successfully.';
+  }
+
+  updateTask() {
+    return 'This task was updated successfully.';
+  }
+
+  deleteTask() {
+    return 'This task was deleted successfully.';
+  }
+
+  updateTaskStatus() {
+    return 'Actualizando el estado de una tarea';
+  }
 }
