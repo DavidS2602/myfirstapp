202391fd07c2883f36eeaee0f410eee8c672aa7d
davids2602 <DavidS260202@proton.me>
2024-08-27 05:05:01 -0600

    Add no-unused-variables config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,14 +21,15 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'semi': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    semi: 'off',
     '@typescript-eslint/semi': 'off',
     'prettier/prettier': [
       'error',
       {
-        'endOfLine': 'auto',
-        'semi': false
-      }
-    ]
-  }
+        endOfLine: 'auto',
+        semi: false,
+      },
+    ],
+  },
 };
