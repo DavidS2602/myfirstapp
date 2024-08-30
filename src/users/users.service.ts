55354d7a361a325c10d10a394df980ac4b43756a
davids2602 <DavidS260202@proton.me>
2024-08-30 05:20:57 -0600

    Add prisma integration to db

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,28 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
 export class UsersService {
-  private users: any[] = [
-    {
-      id: 1,
-      name: 'John',
-      phone: '0123456789',
-    },
-    {
-      id: 2,
-      name: 'Marco',
-      phone: '5513043583',
-    },
-  ];
+  constructor(private prisma: PrismaService) {}
   getUsers() {
-    return this.users;
+    return this.prisma.user.findMany()
   }
 
-  createUsers(user:CreateUserDto) {
-    return {
-      ...user,
-      id: this.users.length + 1,
-    };
+  createUsers(user: CreateUserDto) {
+    return this.prisma.user.create({ data: user })
   }
 }
